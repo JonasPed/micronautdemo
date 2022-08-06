40d11d6cc59614a7095d433d7120e4815ac4bb69
Jonas Pedersen <JonasPed>
2022-08-06 20:51:46 +0000

    Initial commit

---
+++ src/main/java/eu/jope/Application.java
@@ -0,0 +1,9 @@
+package eu.jope;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
